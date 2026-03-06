ed684477a8892f5f88e5d96ef42c87df2f52ce57
Serhan Başözmen <serhan_basozmennn76@hotmail.com>
2026-03-06 22:11:22 +0300

    dotnet dynamic

--- dotnet-basics/obj/Debug/net10.0/dotnet-basics.AssemblyInfo.cs
+++ dotnet-basics/obj/Debug/net10.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5076d220e1519f4c8b8c592ba0e7524ef984acf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166bfecb9b58b9a60e406dbd17e56b3f2fd6e8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
